c2ef9d49643f23301f7f74b87fd0d3834f743f34
adityaslyf <aditya.varshneymail@gmail.com>
2024-03-08 02:50:43 +0530

    added icons to timeline

--- src/components/timeline/timeline.jsx
+++ src/components/timeline/timeline.jsx
@@ -7,6 +7,7 @@ import TimelineImage from '../../assets/timeline/Timeline.png'
 import ArrowImage from '../../assets/timeline/underline(1).png'
 import EndImage from '../../assets/timeline/route.png'
 import StartImage from '../../assets/timeline/start.png'
+import { FaRegClock } from 'react-icons/fa';
 const Timeline = () => {
   const event = [
     {
@@ -58,48 +59,37 @@ const Timeline = () => {
   return (
     <div className='main h-auto' style={{ backgroundImage: `url(${BgImage})`, backgroundSize: 'cover',  backgroundRepeat: "no-repeat",
     backgroundPosition: "bottom", }}>
-        {/* <center><h1 className="text-white text-center font-[Bluu Next] text-[75px] leading-[90px] font-semibold tracking-normal">
-  TIMELINE
-</h1></center> */}
-<center><img className=' w-[293px] ' src={TimelineImage} alt="" />
-<img className=' sm:w-[453px] w-[400px] mt-[30px]' src={ArrowImage} alt="" />
-
-</center>
-
-<div className=''>
-
-{/* <div className='flex justify-center mb-3 '>
-     <img className='filter invert w-12 ml-[px]' src={EndImage} alt="" />
-   </div> */}
-<VerticalTimeline>
-           
-           {event.map((data, index) => {
-              return <VerticalTimelineElement
-              key={index}
-              className="vertical-timeline-element"
-              contentStyle={{ background: '#1C1C1F', color: '#fff'}}
-              contentArrowStyle={{ borderRight: '7px solid #1C1C1F'}}
-              date={data.date}
-              iconStyle={{background: "#1C1C1F"}}
-              dateClassName="date"
-            >
-             <div className='flex justify-around align-center motto_box'>
-              <h2 className="vertical-timeline-element-title">{data.time}</h2>
-              <h2 className='timeline_motto'>
-              {data.motto}  
-              </h2>
-             </div>
-            </VerticalTimelineElement>
-           })}    
-           </VerticalTimeline>
+        <center>
+            <img className=' w-[293px] ' src={TimelineImage} alt="" />
+            <img className=' w-[453px] mt-[30px]' src={ArrowImage} alt="" />
+        </center>
+        <div className=''>
+            <VerticalTimeline>
+                {event.map((data, index) => {
+                    return <VerticalTimelineElement
+                    key={index}
+                    className="vertical-timeline-element"
+                    contentStyle={{ background: '#1C1C1F', color: '#fff'}}
+                    contentArrowStyle={{ borderRight: '7px solid #1C1C1F'}}
+                    date={data.date}
+                    iconStyle={{background: "#1C1C1F"}}
+                    dateClassName="date"
+                    icon={<FaRegClock />}
+                    >
+                    <div className='flex justify-around align-center motto_box'>
+                        <h2 className="vertical-timeline-element-title">{data.time}</h2>
+                        <h2 className='timeline_motto'>
+                        {data.motto}  
+                        </h2>
+                    </div>
+                    </VerticalTimelineElement>
+                })}    
+            </VerticalTimeline>
             <div className='flex 2xl:justify-center  mb-3 '>
-        <img className='filter invert w-12 sm
-        sm:ml-[37px] md:ml-[35px] ml-6 ' src={EndImage} alt="" />
-    </div>
-    
-</div>
-
-        
+                <img className='filter invert w-12 sm
+                sm:ml-[37px] md:ml-[35px] ml-6' src={EndImage} alt="" />
+            </div>
+        </div>
     </div>
   )
 }
